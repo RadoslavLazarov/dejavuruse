a8a0bd4bb4ea097b719ae6259f027be2914c7d14
alien.lazarov@gmail.com <alien.lazarov@gmail.com>
2019-05-30 13:06:50 +0300

    add mobile menu

--- static/js/test.js
+++ static/js/test.js
@@ -3,5 +3,15 @@ $(function () {
         $('#loading-screen').fadeOut("slow");
     }, 500);
 
+    $('.nav-button').on('click', function () {
+        $('.main-nav-wrapper').toggleClass('d-none');
+    });
+
+    $('.nav-icon').click(function () {
+        $(this).toggleClass('open');
+    });
+
     $('.onload').addClass('loaded');
+
+
 });
