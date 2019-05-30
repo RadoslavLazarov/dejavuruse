fd6b444eeaf1409ab80ab3e1682848537f8e2d23
alien.lazarov@gmail.com <alien.lazarov@gmail.com>
2019-05-30 13:45:14 +0300

    messenger test

--- static/js/thirdParty/messenger.js
+++ static/js/thirdParty/messenger.js
@@ -1,32 +1,15 @@
-// window.fbAsyncInit = function () {
-//     FB.init({
-//         xfbml: true,
-//         version: 'v3.3'
-//     });
-// };
-
-// (function (d, s, id) {
-//     var js, fjs = d.getElementsByTagName(s)[0];
-//     if (d.getElementById(id)) return;
-//     js = d.createElement(s); js.id = id;
-//     js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v3.3&autoLogAppEvents=1';
-//     fjs.parentNode.insertBefore(js, fjs);
-// }(document, 'script', 'facebook-jssdk'));
-
-
 window.fbAsyncInit = function () {
     FB.init({
-        appId: '449532792515169',
-        autoLogAppEvents: true,
         xfbml: true,
-        version: 'v2.11'
+        version: 'v3.3'
     });
 };
+
 (function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
+    if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
