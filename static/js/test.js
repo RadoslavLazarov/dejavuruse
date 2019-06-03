bafbf17d78c78c239fe57ba03543806b8401a016
alien.lazarov@gmail.com <alien.lazarov@gmail.com>
2019-06-03 18:00:18 +0300

    test

--- static/js/test.js
+++ static/js/test.js
@@ -1,11 +1,5 @@
 /* eslint-disable */
 $(function () {
-    window.addEventListener('resize', () => {
-        // We execute the same script as before
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-
     setTimeout(function () {
         $('#loading-screen').fadeOut("slow");
     }, 500);
