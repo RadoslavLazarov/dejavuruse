a4e6af9396eb25bd261024f0899e86a687d843f3
alien.lazarov@gmail.com <alien.lazarov@gmail.com>
2019-06-03 17:12:40 +0300

    animate mobile menu

--- static/js/test.js
+++ static/js/test.js
@@ -1,14 +1,22 @@
+/* eslint-disable */
 $(function () {
     setTimeout(function () {
         $('#loading-screen').fadeOut("slow");
     }, 500);
 
     $('.nav-button').on('click', function () {
-        // $('.main-nav-wrapper').toggleClass('d-none');
-        $('nav[role="main-nav"]').toggle();
-        $('.header-top').toggleClass('d-none');
-        $('.logo').toggle();
+        const navigation = $('nav[role="main-nav"]');
+
+        if (navigation.css('visibility') === 'hidden') {
+            navigation.css({ 'visibility': 'visible' })
+        } else if (navigation.css('visibility') === 'visible') {
+            setTimeout(function () {
+                navigation.css({ 'visibility': 'hidden' })
+            }, 500);
+        }
+
         $('.header-bottom').toggleClass('opened');
+        $(this).toggleClass('opened');
     });
 
     $('.nav-icon').click(function () {
@@ -16,6 +24,5 @@ $(function () {
     });
 
     $('.onload').addClass('loaded');
-
 
 });
