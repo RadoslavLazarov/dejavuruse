0ff0949536bfc5119cdfd0c73454ef0666e119b2
alien.lazarov@gmail.com <alien.lazarov@gmail.com>
2019-07-02 10:17:44 +0300

    home page summary hide under 576 max-height

--- static/js/index.js
+++ static/js/index.js
@@ -79,7 +79,7 @@ require('./functions');
 require('./events');
 require('./onLoad');
 
-}).call(this,require("e/U+97"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_17affbbe.js","/")
+}).call(this,require("e/U+97"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_a1a64ce5.js","/")
 },{"./events":1,"./functions":3,"./onLoad":4,"./thirdParty/aos":5,"./thirdParty/googleMaps":6,"./thirdParty/jquery":7,"./thirdParty/photoswipe":8,"buffer":11,"e/U+97":16}],3:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 /* eslint-disable */
