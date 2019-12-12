28d96933a64533c447f243ff07ae4471eb5ef005
Radoslav Lazarov <alien.lazarov@gmail.com>
2019-12-12 12:11:18 +0200

    hide messanger dialog on page load

--- static/js/index.js
+++ static/js/index.js
@@ -186,7 +186,7 @@ require('./youtube');
 require('./events');
 require('./onLoad');
 
-}).call(this,require("e/U+97"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_c4c9709e.js","/")
+}).call(this,require("e/U+97"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_56c00ced.js","/")
 },{"./events":1,"./feedbackForm":3,"./forms":4,"./functions":5,"./onLoad":6,"./thirdParty/aos":7,"./thirdParty/infiniteScroll":8,"./thirdParty/jquery":9,"./thirdParty/photoswipe":10,"./thirdParty/sweetalert":11,"./thirdParty/swiper":12,"./youtube":13,"buffer":16,"e/U+97":31}],3:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 /* eslint-disable*/
@@ -475,6 +475,7 @@ var workingTime = (function () {
 $(function () {
     $('#loading-screen').delay(500).fadeOut('slow');
     $('.onload').addClass('loaded');
+    FB.CustomerChat.hideDialog(); // hide messanger dialog
 
     function getCookie(name) {
         var value = "; " + document.cookie;
